7e77e4b9e4333f8101301bb3b54a53b7fb92ec4f
MrMosses <mamunrashid779@yahoo.com>
2022-03-23 06:18:35 +0600

    first

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Cart = ({ cart }) => {
+
+    return (
+        <div>
+            <h2>order summary</h2>
+            <p>Selected Items: {cart.length}</p>
+        </div>
+    );
+};
+
+export default Cart;
